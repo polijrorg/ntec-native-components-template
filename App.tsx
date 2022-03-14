ec91f7adf6dc1d3c97dd5e5dfaeeffde41923a0a
Henrique <henrique@falconer.com.br>
2022-03-13 21:26:56 -0300

    :package: package: add @ntec/native

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { DropDown } from "@ntec/native";
+import { useState } from "react";
 
 export default function App() {
+  const [selectedOption, setSelectedOption] = useState("Opção 1");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Um componente do @ntec/native funcionando no seu app!{"\n"}</Text>
+      <DropDown
+        label="Selecione a opção"
+        disableSelectAll
+        value={selectedOption}
+        onChange={setSelectedOption}
+        options={["Opção 1", "Opção 2"]}
+      />
     </View>
   );
 }
@@ -13,8 +24,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 42
   },
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          extensions: [".ts", ".tsx", ".jsx", ".js", ".json"],
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
